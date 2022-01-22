adabae8893c7e9364daefbb5faa979885eedf466
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-22 20:54:29 +0530

    Row and col wise sorted matrix

---
+++ 05. Problem Solving on 2D Arrays/MaximumSumSubmatrixInRowColumnwiseSortedMatrix.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int maxsumsubmatrix(int **arr, int n, int m)
+{
+    for(int i = n-1; i >= 0; i--)
+    {
+        for(int j = m-2; j >= 0; j--)
+        {
+            arr[i][j] += arr[i][j+1];
+        }
+    }
+    for(int i = m-1; i >= 0; i--)
+    {
+        for(int j = n-2; j >= 0; j--)
+        {
+            arr[j][i] += arr[j+1][i];
+        }
+    }
+    int result = INT_MIN;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            result = max(result, arr[i][j]);
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    int **arr = new int*[n];
+    for(int i = 0; i < n ; i++)
+    {
+        arr[i] = new int[m];
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"Maximum Sum submatrix "<<maxsumsubmatrix(arr, n, m);
+    return 0;
+}
